de5f169d3a080389098b95f910e7b04ef0608e94
Roman <devilog@rambler.ru>
2020-08-23 14:41:43 +0300

    Added object check

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -7,16 +7,28 @@ import {ListGroup, ListGroupItem} from 'reactstrap';
 const PostList = ({posts}) => {
 
     const elements = posts.map((item) => {
-        const {id, ...itemProps} = item;
-        return (
-            <li key={id} className='list-group-item'>
-                <PostListItem {...itemProps}/>
-                {/* <PostListItem 
-                label={item.label}
-                important={item.important} /> */}
-            </li>
-        )
-    });
+        if ( typeof item === 'object' && notEmpty (item) ) {
+            const {id, ...itemProps} = item;
+            return (
+                <li key={id} className='list-group-item'>
+                    <PostListItem {...itemProps}/>
+                    {/* <PostListItem 
+                    label={item.label}
+                    important={item.important} /> */}
+                </li>
+            )    
+        }        
+        
+    })
+
+    function notEmpty(obj) {
+        for (let key in obj) 
+        {
+            // если тело цикла начнет выполняться - значит в объекте есть свойства
+            return true;
+        }
+        return false;
+    }
 
     return (
         <ListGroup className="app-list list-group">
