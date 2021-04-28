1343827acc57e2eb85e6972f62398ec7ed801996
Roman <rimer93@gmail.com>
2021-04-28 15:05:29 +0300

    Updates

---
+++ precache-manifest.ae5d790cd72fc61595fa61ee3910809c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39cd1bb5bb780c5490c81aa05b7b1251",
+    "url": "/simple-blog-spa/index.html"
+  },
+  {
+    "revision": "a0bf6f68d86b4f8efda2",
+    "url": "/simple-blog-spa/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "b6fafdac474125f91700",
+    "url": "/simple-blog-spa/static/css/main.267dbf8c.chunk.css"
+  },
+  {
+    "revision": "a0bf6f68d86b4f8efda2",
+    "url": "/simple-blog-spa/static/js/2.a5035d7a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/simple-blog-spa/static/js/2.a5035d7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6fafdac474125f91700",
+    "url": "/simple-blog-spa/static/js/main.8270ac6c.chunk.js"
+  },
+  {
+    "revision": "77384fef8138bc09873b",
+    "url": "/simple-blog-spa/static/js/runtime-main.4c52cd3b.js"
+  }
+]);
